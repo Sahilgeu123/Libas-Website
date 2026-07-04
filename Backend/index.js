717e6402c16ab255f066f575fdc09d535ee2de86
Sahilgeu123 <145110017+Sahilgeu123@users.noreply.github.com>
2026-07-04 13:40:48 +0530

    deleted clean-repo

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-dotenv.config();
+const connectDB = require("./config/db");
+
+dotenv.config(); 
+connectDB();
 
 const app = express();
 app.use(cors());
