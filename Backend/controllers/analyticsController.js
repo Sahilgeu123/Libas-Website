35523fe5410057aecc52ad6b3bff5282b38c0c10
Sahilgeu123 <145110017+Sahilgeu123@users.noreply.github.com>
2026-08-09 11:44:46 +0530

    implemented analyitcs routes and controller

--- Backend/controllers/analyticsController.js
+++ Backend/controllers/analyticsController.js
@@ -4,14 +4,14 @@ const Product = require('../models/productModel.js');
 
 const getAdminStats = async (req, res) => {
     try {
-        const totalUsers = await User.countDocuments({role: 'user'});
+        const totalUsers = await User.countDocuments();
         const totalOrders = await Order.countDocuments();
         const totalProducts = await Product.countDocuments();
         
         const orders = await Order.find();
 
-        const totalRevenueData = await Order.reduce((acc, order) => {
-            return acc + order.totalPrice;
+        const totalRevenueData = await orders.reduce((acc, order) => {
+            return acc + order.totalAmount;
         }, 0);
         const totalRevenue = totalRevenueData[0]?.total || 0;
         res.status(200).json({
